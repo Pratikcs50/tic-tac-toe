ec7dfb400b338eb0b857581afb58f05d8f933270
pratik <pratikofficial26@gmail.com>
2022-12-20 16:34:05 +0530

    added custom theme for mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.scss'
+import * as React from "react";
+import "./App.scss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F2C94C",
+    },
+    secondary: {
+      main: "#2F80ED",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      HELLO World
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+
+      </div>
+    </ThemeProvider>
   );
 }
 
