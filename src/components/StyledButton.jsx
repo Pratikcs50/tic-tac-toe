bf55c1362e71e62ecc90f41bdb3b57fc38c451a6
pratik <pratikofficial26@gmail.com>
2022-12-20 17:28:31 +0530

    created custom styled component

---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Button
+} from '@mui/material'
+
+function StyledButton(props) {
+  const { styles, title } = props;
+  return (
+    <Button
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#FFFFFF",
+        height: "56px",
+        width: "328px",
+        fontSize: "14px",
+        fontWeight: "700",
+        fontFamily: "Epilogue",
+        padding: "10px 16px 2px 16px",
+        ...styles,
+      }}
+      variant="contained"
+    >
+      {title}
+    </Button>
+  );
+}
+
+export default StyledButton;
