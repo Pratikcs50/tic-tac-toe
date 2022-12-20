c64ef7c90f71ae6dac761402984dc66fe890dad3
pratik <pratikofficial26@gmail.com>
2022-12-20 17:48:15 +0530

    developed and styled Entry Page

--- src/layout/entry/Entry.jsx
+++ src/layout/entry/Entry.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Entry.scss'
+import StyledButton from '../../components/StyledButton'
 
 function Entry() {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="text_container">
+        <div className="sub_heading">asyn</div>
+        <div className="heading">tic tac toe</div>
+      </div>
+      <div className="action_container">
+      <StyledButton title="Login" />
+      <StyledButton title="Register" color="secondary"/>
+      </div>
     </div>
   )
 }
