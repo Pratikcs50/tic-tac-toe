f8cecbf55832be28b6f6b09d73164c097042a478
pratik <pratikofficial26@gmail.com>
2022-12-20 15:59:28 +0530

    created new game page in pages directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-
+      HELLO World
     </div>
   );
 }
---
+++ src/pages/NewGamePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewGamePage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewGamePage
