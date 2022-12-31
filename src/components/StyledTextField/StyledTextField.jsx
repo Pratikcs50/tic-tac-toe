1bcdd0d69cd4830b47f7a0c3c0526838d1065f71
Pratikcs50 <pratikofficial26@gmail.com>
2022-12-31 17:20:58 +0530

    used div instead of react fragment in styled text field component

--- src/components/StyledTextField/StyledTextField.jsx
+++ src/components/StyledTextField/StyledTextField.jsx
@@ -5,10 +5,10 @@ import "./StyledTextField.scss"
 function StyledTextField(props) {
   const { inputTitle, type, placeholder } = props;
   return (
-    <>
+    <div>
       <InputLabel className='input_lable'>{inputTitle}</InputLabel>
       <TextField className='input_field' type={type} variant='outlined' placeholder={ placeholder} />
-    </>
+    </div>
   );
 }
 
