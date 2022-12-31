1170b1a29ebf59fbbaf1fe8917016acc03b4e117
Pratikcs50 <pratikofficial26@gmail.com>
2022-12-31 18:51:00 +0530

    developed the Loging page layout and styled as well

--- src/layout/login/Login.jsx
+++ src/layout/login/Login.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import './Login.scss'
+import StyledTextField from '../../components/StyledTextField/StyledTextField';
+import StyledButton from '../../components/StyledButton';
+import ActionBar from '../../components/ActionBar/ActionBar';
 
 function Login() {
   return (
-    <div>
-      
+    <div className='login_container'>
+      <ActionBar to='' />
+      <div className='text_container'>
+        <div className='sub_heading'>Login</div>
+        <div className='heading'>Please enter your details</div>
+      </div>
+      <div className='form_container'>
+        <StyledTextField
+          inputTitle='Username'
+          type='text'
+          placeholder='Type your username here'
+        />
+        <StyledTextField
+          inputTitle='Password'
+          type='password'
+          placeholder='Type your password here'
+        />
+      </div>
+      <div className='action_container'>
+        <StyledButton title='Login' />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
