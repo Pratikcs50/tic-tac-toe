a44a5c639444125d7b7f3a282c664851ca1c1b42
Pratikcs50 <pratikofficial26@gmail.com>
2022-12-31 16:22:45 +0530

    added header and subheader with action button in register layout

--- src/layout/register/Register.jsx
+++ src/layout/register/Register.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Register.scss'
+import StyledButton from '../../components/StyledButton';
 
 function Register() {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='text_container'>
+        <div className='sub_heading'>Create account</div>
+        <div className='heading'>Let's get to know you better!</div>
+      </div>
+      <div className='form_container'>
+        
+      </div>
+      <div className='action_container'>
+        <StyledButton title='Register' />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Register
